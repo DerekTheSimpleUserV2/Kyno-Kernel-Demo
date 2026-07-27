b8d1ca25033dcd8b5090d5b9cbdef6f68e00be29
DerekTheSimpleUserV2 <derektherobloxian@gmail.com>
2026-07-27 12:21:35 -0300

    Add keyboard driver with scancode to ASCII mapping
    Implement keyboard handler and scancode mapping

---
+++ hardware_drivers/keyboard.c
@@ -0,0 +1,17 @@
+// keyboard.c
+#include "keyboard.h"
+#include "screen.h"
+
+unsigned char scancode_to_ascii[128] = {
+    0, 27, '1','2','3','4','5','6','7','8','9','0','-','=', '\b',
+    '\t','q','w','e','r','t','y','u','i','o','p','[',']','\n', 0,
+    'a','s','d','f','g','h','j','k','l',';','\'','`', 0,
+    '\\','z','x','c','v','b','n','m',',','.','/', 0, '*', 0,' '
+};
+
+void keyboard_handler(unsigned char scancode) {
+    char c = scancode_to_ascii[scancode];
+    if (c) {
+        putchar(c);
+    }
+}
